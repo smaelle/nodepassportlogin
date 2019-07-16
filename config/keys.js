0237b6205529343076945951d44fdf530fb751fe
smaelle <smaellel@gmail.com>
2019-07-16 15:26:52 +0200

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    //MongoURI:'mongodb+srv://userfrugal:smaelle@questions-u6nef.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb://127.0.0.1:27017/nodepassportlogin',
+}
